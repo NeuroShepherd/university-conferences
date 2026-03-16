edd45b36bffcb4a457e4e81be1d8e6cf8a26df1a
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-16 08:10:57 +0100

    Continue building testing queries

--- testing.sql
+++ testing.sql
@@ -28,3 +28,29 @@ FROM universities AS u
 LEFT JOIN university_conference_memberships AS mem
 ON u.university_wikipedia_href = mem.university_wikipedia_href
 WHERE u.university_id = 1117;
+
+
+-- find most recent conference membership for university_id 1117
+SELECT *
+FROM universities AS u
+LEFT JOIN university_conference_memberships AS mem
+ON u.university_wikipedia_href = mem.university_wikipedia_href
+WHERE u.university_id = 1117
+	AND mem.start_year = (
+		SELECT MAX(start_year)
+	      FROM university_conference_memberships
+	      WHERE university_wikipedia_href = u.university_wikipedia_href
+	);
+
+
+-- alternative to the previous code, but works properly for multiple schools
+SELECT *
+FROM (
+    SELECT *,
+           MAX(start_year) OVER (PARTITION BY university_wikipedia_href) AS max_start_year
+    FROM university_conference_memberships
+) mem
+JOIN universities u
+  ON u.university_wikipedia_href = mem.university_wikipedia_href
+WHERE u.university_id IN (1117, 1118)
+  AND mem.start_year = mem.max_start_year;
